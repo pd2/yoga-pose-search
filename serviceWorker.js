49ea48c17d720b4b82bfe3058e85716ac2374294
Glitch (yoga-pose-search) <none>
2025-05-26 15:49:26 +0000

    ◻️🛕 Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDev = "yoga_pose_search"
+const service_worker_version = "v1.4"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
